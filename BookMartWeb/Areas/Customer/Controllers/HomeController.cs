918f100f73677fff883b6ed4c3974f2ba43868b0
navin <navindalmia2003@yahoo.co.in>
2024-07-08 18:58:06 +0100

    Home and Details page updated

--- BookMartWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookMartWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using BookMart.DataAccess.Repository;
+using BookMart.DataAccess.Repository.IRepository;
 using BookMart.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,17 +10,26 @@ namespace BookMartWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork, ILogger<HomeController> logger)
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Product> objProductList = _unitOfWork.ProductRepository.GetAll("Category").ToList();
+            return View(objProductList);
+            
         }
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.ProductRepository.Get(u=>u.Id==id,"Category");
+            return View(product);
 
+        }
         public IActionResult Privacy()
         {
             return View();
