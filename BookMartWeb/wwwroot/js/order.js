590e1a55b633ac502ffd0ca1745dda6dd28b27de
navin <navindalmia2003@yahoo.co.in>
2025-02-01 15:35:01 +0000

    controller and index view for Order List

---
+++ BookMartWeb/wwwroot/js/order.js
@@ -0,0 +1,41 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+    
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable(
+        {
+            "ajax": { url:'/admin/order/getall'},
+            "columns": [
+                { data: 'id', "width": "5%" },
+                { data: 'name', "width": "10%" },
+                { data: 'phoneNumber', "width": "10%" },
+                { data: 'applicationUser.email', "width": "15%" },
+                { data: 'orderStatus', "width": "10%" },
+                { data: 'orderTotal', "width": "10%" },
+                {
+                    data: 'id',
+                    "render": function (data) {
+                        return `<div class="w-75 btn-group" role="group"> 
+
+                        <a href="/admin/order/details?id=${data}" class="btn btn-primary mx-2">
+                        <i class="bi bi-pencil-square"> </i> Edit </a>
+                        
+                        </div>`
+                    }
+                    , "width": "20%"
+                } 
+                
+              
+               
+            ]
+
+        }
+    );
+
+
+
+}
+
