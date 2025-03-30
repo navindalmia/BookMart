eab38417cf0b3076575ea4fef76ac9e0312c83cb
navin <navindalmia2003@yahoo.co.in>
2025-03-30 15:31:58 +0100

    Change to Ordder Summary Screen

--- BookMartWeb/Areas/Admin/Controllers/OrderController.cs
+++ BookMartWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,10 @@
 ﻿using BookMart.DataAccess.Repository.IRepository;
 using BookMart.Models;
+using BookMart.Models.ViewModels;
+using BookMart.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BookMartWeb.Areas.Admin.Controllers
 {
@@ -17,12 +20,44 @@ namespace BookMartWeb.Areas.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Details( int orderId)
+        {
+            OrderVM orderVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeaderRepository.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetailRepository.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+
+            };
+
+            return View(orderVM);
+        }
         #region API Calls
 
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where( u=> u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+
             return Json(new { data = objOrderHeaders });
 
         }
--- BookMartWeb/wwwroot/js/order.js
+++ BookMartWeb/wwwroot/js/order.js
@@ -1,16 +1,40 @@
 ﻿var dataTable;
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+
+        }
+
+    }
+    //loadDataTable();
     
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable(
         {
-            "ajax": { url:'/admin/order/getall'},
+            "ajax": { url:'/admin/order/getall?status=' + status},
             "columns": [
                 { data: 'id', "width": "5%" },
-                { data: 'name', "width": "10%" },
+                { data: 'name', "width": "20%" },
                 { data: 'phoneNumber', "width": "10%" },
                 { data: 'applicationUser.email', "width": "15%" },
                 { data: 'orderStatus', "width": "10%" },
@@ -20,12 +44,12 @@ function loadDataTable() {
                     "render": function (data) {
                         return `<div class="w-75 btn-group" role="group"> 
 
-                        <a href="/admin/order/details?id=${data}" class="btn btn-primary mx-2">
+                        <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2">
                         <i class="bi bi-pencil-square"> </i> Edit </a>
                         
                         </div>`
                     }
-                    , "width": "20%"
+                    , "width": "10%"
                 } 
                 
               
